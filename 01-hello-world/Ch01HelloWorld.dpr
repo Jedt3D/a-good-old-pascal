8f9c3150873536a13023092fcf434c5d44f14f04
Worajedt Sitthidumrong <sjedt@3ddaily.com>
2024-09-17 18:10:54 +0700

    simple hello world

---
+++ 01-hello-world/Ch01HelloWorld.dpr
@@ -0,0 +1,14 @@
+program Ch01HelloWorld;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ 01-hello-world/MainUnit.pas
@@ -0,0 +1,32 @@
+unit MainUnit;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Mask, Vcl.ExtCtrls;
+
+type
+  TfrmMain = class(TForm)
+    edlbName: TLabeledEdit;
+    btnSayHi: TButton;
+    procedure btnSayHiClick(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  frmMain: TfrmMain;
+
+implementation
+
+{$R *.dfm}
+
+procedure TfrmMain.btnSayHiClick(Sender: TObject);
+begin
+  edlbName.Text := 'Hello, World';
+end;
+
+end.
